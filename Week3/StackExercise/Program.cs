f898484e858fdf55c4c4b70a3ffc16e0179601ca
AndreasVikke <andreasvikke@gmail.com>
2021-02-09 11:45:47 +0100

    Uncommentet Test

--- Week3/StackExercise/Program.cs
+++ Week3/StackExercise/Program.cs
@@ -8,41 +8,42 @@ namespace StackExercise
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine("========== ArrayStack Test ==========");
-            // ArrayStack<int> ast = new ArrayStack<int>(10);
+            Console.WriteLine("========== ArrayStack Test ==========");
+            ArrayStack<int> ast = new ArrayStack<int>(10);
 
-            // Console.WriteLine("Adds 1 to stack");
-            // ast.push(1);
-            // Console.WriteLine("Peek stack: " + ast.peek());
-            // Console.WriteLine("Pop stack: " + ast.pop());
-            // Console.WriteLine("Adds 2 to stack");
-            // Console.WriteLine("Adds 5 to stack");
-            // Console.WriteLine("Adds 9 to stack");
-            // ast.push(2);
-            // ast.push(5);
-            // ast.push(9);
-            // Console.WriteLine("Peek stack: " + ast.peek());
-            // Console.WriteLine("Pop stack: " + ast.pop());
-            // Console.WriteLine("Peek stack: " + ast.peek());
+            Console.WriteLine("Adds 1 to stack");
+            ast.push(1);
+            Console.WriteLine("Peek stack: " + ast.peek());
+            Console.WriteLine("Pop stack: " + ast.pop());
+            Console.WriteLine("Adds 2 to stack");
+            Console.WriteLine("Adds 5 to stack");
+            Console.WriteLine("Adds 9 to stack");
+            ast.push(2);
+            ast.push(5);
+            ast.push(9);
+            Console.WriteLine("Peek stack: " + ast.peek());
+            Console.WriteLine("Pop stack: " + ast.pop());
+            Console.WriteLine("Peek stack: " + ast.peek());
 
 
-            // Console.WriteLine("========== LinkedStack Test ==========");
-            // LinkedStack<int> lst = new LinkedStack<int>();
+            Console.WriteLine("========== LinkedStack Test ==========");
+            LinkedStack<int> lst = new LinkedStack<int>();
 
-            // Console.WriteLine("Adds 1 to stack");
-            // lst.push(1);
-            // Console.WriteLine("Peek stack: " + lst.peek());
-            // Console.WriteLine("Pop stack: " + lst.pop());
-            // Console.WriteLine("Adds 2 to stack");
-            // Console.WriteLine("Adds 5 to stack");
-            // Console.WriteLine("Adds 9 to stack");
-            // lst.push(2);
-            // lst.push(5);
-            // lst.push(9);
-            // Console.WriteLine("Peek stack: " + lst.peek());
-            // Console.WriteLine("Pop stack: " + lst.pop());
-            // Console.WriteLine("Peek stack: " + lst.peek());
+            Console.WriteLine("Adds 1 to stack");
+            lst.push(1);
+            Console.WriteLine("Peek stack: " + lst.peek());
+            Console.WriteLine("Pop stack: " + lst.pop());
+            Console.WriteLine("Adds 2 to stack");
+            Console.WriteLine("Adds 5 to stack");
+            Console.WriteLine("Adds 9 to stack");
+            lst.push(2);
+            lst.push(5);
+            lst.push(9);
+            Console.WriteLine("Peek stack: " + lst.peek());
+            Console.WriteLine("Pop stack: " + lst.pop());
+            Console.WriteLine("Peek stack: " + lst.peek());
 
+            Console.WriteLine("========== LinkedQueue Test ==========");
             LinkedQueue<int> lqu = new LinkedQueue<int>();
 
             Console.WriteLine("Enqueues 1 to queue");
